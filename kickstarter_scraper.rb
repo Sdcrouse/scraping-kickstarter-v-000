296930439934de4bdf367288a477baf8e2d6f065
Steven Crouse <sdcrouse91@gmail.com>
2018-09-20 06:38:31 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -20,7 +20,10 @@ def create_project_hash
   projects
 end 
 
-create_project_hash.each {|title, hash| puts "#{title}: #{hash}"}
+create_project_hash.each do |title, hash| 
+  puts "#{title}:"
+  hash.each {|key, value| puts "#{key}: #{value}"}
+end
 
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
